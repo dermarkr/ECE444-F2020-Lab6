a968b49e63027dfe769ecd4a7c04dbc57ed825d1
dermarkr <45435121+dermarkr@users.noreply.github.com>
2020-10-30 14:14:07 -0400

    Added database and related tests and connections

---
+++ tests/app_test.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from project.app import app
+
+
+def test_index():
+    tester = app.test_client()
+    response = tester.get("/", content_type="html/text")
+
+    assert response.status_code == 200
+    assert response.data == b"Hello, World!"
+
+
+def test_database():
+    assert Path("flaskr.db").is_file()
